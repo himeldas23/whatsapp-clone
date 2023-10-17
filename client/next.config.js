65c057afc17005210ae262804e4baab15d028ef5
Himel Das <112402957+himeldas23@users.noreply.github.com>
2023-10-17 20:15:30 +0600

    Integrating-ZegoCloud-Setup

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  env: {
+    NEXT_PUBLIC_ZEGO_APP_ID: 1350416234,
+    NEXT_PUBLIC_ZEGO_SERVER_ID : "1d73c2706f5e30c0fd05e43da52b54b4"
+  },
   images: {
     domains:["localhost"],
   },
